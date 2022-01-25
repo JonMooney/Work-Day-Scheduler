8282b14d3742d3dd8421f89d430461a54102e28f
Jon Mooney <jon.mooney@yahoo.com>
2022-01-24 17:28:54 -0800

    added JS for colored bg based on hours

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,21 @@
 //Script file
 
 
-console.log(moment().hour())
+//console.log(moment().hour())
 
 //Place today's date at the top of the page
 document.getElementById("currentDay").innerHTML = moment().format("dddd, MMMM Do");
 
-
-
-// var newEl;
-// var ampm = "AM";
-
-// for(var i=9;i<=17;i++){
-//     if(i>11){
-//         ampm = "PM";
-//     }
-
-//     newEl = "<div class='timeblock row'>";
-//     newEl += "<div class='col-1 hour'>" + i + ampm + "</div>";
-//     newEl += "<div id='description-" + i + "' class='col-10 description'></div>";
-//     newEl += "<div id='save-9' class='col-1 saveBtn d-flex justify-content-center align-items-center'><i class='fas fa-save'></i></div></div>";
-//     $(".container").append(newEl);
-// }
-
-//var newEl = "<div class='timeblock row'><div class='col-1 hour'>" + i + ampm + "</div>";
-
+//Add colored backgrounds for past, current, and future hours
+for(i=9;i<=17;i++){
+    if(i < moment().hour()){
+        console.log("before");
+        $("#description-" + i).addClass("past");
+    }else if(i == moment().hour()){
+        console.log("current hour - " + i);
+        $("#description-" + i).addClass("present");
+    }else{
+        console.log("upcoming hour");
+        $("#description-" + i).addClass("future");
+    }
+}
