3e75d2545788ded03abadd6c060d7eed7711d44f
Jon Mooney <jon.mooney@yahoo.com>
2022-01-24 19:19:08 -0800

    created test JS that turns description area into textarea

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 //Script file
 
-
-//console.log(moment().hour())
+var schedule = [];
 
 //Place today's date at the top of the page
 document.getElementById("currentDay").innerHTML = moment().format("dddd, MMMM Do");
@@ -19,3 +18,23 @@ for(i=9;i<=17;i++){
         $("#description-" + i).addClass("future");
     }
 }
+
+//Function to save entered text to localStorage
+$(".container").on("click", ".saveBtn", function(){
+    console.log($(this).attr("id"));
+});
+
+$("#description-10").click(function(){
+    //console.log("clicked on 9AM");
+    //$(this).html("<textarea></textarea>");
+    //$("#textarea-10").addClass("show");
+    //var newEl = $(document)
+    console.log($("#textarea-10").length);
+    if(!$("#textarea-10").length){
+        var newEl = document.createElement("textarea");
+        $(newEl).attr("id", "textarea-10")
+        $(this).append(newEl);
+        newEl.focus();
+        console.log("creating textbox");
+    }
+});
